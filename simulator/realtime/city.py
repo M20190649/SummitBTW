473d0de92c171ca2b929c4e1a5f4c989893dbe78
EylonSho <eylonsho999@gmail.com>
2017-12-26 01:07:38 +0200

    Organize modules and changing names according to convention.

--- simulator/realtime/city.py
+++ simulator/realtime/city.py
@@ -11,7 +11,7 @@ from .junction import Junction
 
 
 class City(object):
-    """A city, containing all of the traffic lights currently loaded to the Simu.
+    """A city, containing all of the traffic lights currently loaded to the simulator.
 
     """
     def __init__(self):
