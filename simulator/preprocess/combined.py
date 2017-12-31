74c5c39d2a2a667488a8a19844a15fef1a16631b
EylonSho <eylonsho999@gmail.com>
2017-12-31 20:59:56 +0200

    Auto create .sumocfg.xml file to the preprocessing stage of simulations #85.

--- simulator/preprocess/combined.py
+++ simulator/preprocess/combined.py
@@ -3,13 +3,28 @@ Combining all of the preprocessing parts of simulation creation into a single un
 This script offers all necessary files for SUMO simulation,
 including SUMO network, traffic lights, detectors and vehicles trips.
 """
+__author__ = "Eylon Shoshan"
 import logging
+import os
+import xml.etree.cElementTree as et
+from xml.etree.ElementTree import Element, SubElement
 
 from simulator.preprocess.detectors import add_detectors
 from simulator.preprocess.network import generate_net
 from simulator.preprocess.random_trips import generate_trips
 
-__author__ = "Eylon Shoshan"
+
+def _configuration_file(path):
+    config_xml = Element('configuration')
+
+    input_element = SubElement(config_xml, "input")
+    SubElement(input_element, "net-file").set("value", "city.net.xml")
+    SubElement(input_element, "route-files").set("value", "routes.rou.alt.xml")
+    SubElement(input_element, "additional-files").set("value", "e2.add.xml")
+
+    config_path = os.path.join(path, "city.sumocfg.xml")
+    config_tree = et.ElementTree(config_xml)
+    config_tree.write(config_path)
 
 
 def prepare_simulation(path="."):
@@ -25,5 +40,6 @@ def prepare_simulation(path="."):
     generate_net(sumo_network)
     add_detectors(sumo_network)
     generate_trips(sumo_network, path)
+    _configuration_file(path)
 
     logging.info('Finished to prepare simulation. All relevant files were created.')
