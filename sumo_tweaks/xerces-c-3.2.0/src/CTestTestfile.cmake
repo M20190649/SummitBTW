30d6603d9abdbcdd00226272831af11acbb6a100
Dor Granat <gdor80@gmail.com>
2018-01-13 18:00:22 +0200

    deleted redundant xercesc files

--- sumo_tweaks/xerces-c-3.2.0/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gdor80/Desktop/SummitBTW/sumo/xerces-c-3.2.0/src
-# Build directory: /home/gdor80/Desktop/SummitBTW/sumo/xerces-c-3.2.0/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
