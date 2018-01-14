5aefd1c5fc0c1f3894e8fad4d6546800bf863617
Tomer Kay <tomerkay@gmail.com>
2018-01-14 23:53:30 +0200

    currently working with sock script #103 #106 #107

--- statistics/rt_sock.py
+++ statistics/rt_sock.py
@@ -1,33 +1,31 @@
-import socket   #for sockets
-import sys  #for exit
- 
-#create an INET, STREAMing socket
+import socket  # for sockets
+import sys  # for exit
+
+# create an INET, STREAMing socket
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket()
 except socket.error:
-    print 'Failed to create socket'
+    print('Failed to create socket')
     sys.exit()
-print 'Socket Created'
- 
-host = '127.0.0.1';
-port = 1482;
- 
-#Connect to remote server
+print('Socket Created')
+
+host = '127.0.0.1'
+port = 1482
+
+# Connect to remote server
 try:
-    s.connect((host , port))
+    s.connect((host, port))
 except Exception as e:
     print(e)
-#Send some data to remote server
+# Send some data to remote server
 while True:
     id_desired = input("## Write id of object you are interested for >>\n")
-    try :
-        #Set the whole string
-        s.sendall(str(id_desired))
+    try:
+        # Set the whole string
+        s.sendall(str.encode(str(id_desired)))
     except socket.error as e:
-        #Send failed
-        print e
+        # Send failed
+        print(e)
         sys.exit()
-    print 'Message send successfully'
-     
     reply = s.recv(4096)
-    print reply
+    print(reply.decode("utf-8"))
