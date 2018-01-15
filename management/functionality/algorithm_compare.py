0dc90212b67f07fd69a82297e20ba1e484bd8e2a
EylonSho <eylonsho999@gmail.com>
2018-01-15 18:14:01 +0200

    Adjusting to performing a simulation with the example name straight forwardly, according to the change in #114

--- management/functionality/algorithm_compare.py
+++ management/functionality/algorithm_compare.py
@@ -2,16 +2,16 @@ import os
 from sys import argv
 
 from scheduler.scheduler_constants import schedulers_name_map
-from simulator.simulate import run_sumo
+from simulator.simulate import run_sumo, run_simulation_example
 from statistics.get_statistics import create_statistics
 
 TEMP_OUT_DIR = "./temp_outputs"
 
 
-def compare_algorithms(config_file, *scheduler_names):
+def compare_algorithms(simulation_example, *scheduler_names):
     """
     runs a simulation with both schedulers and compares their performance
-    :param config_file: the config file for running the simulation
+    :param simulation_example: the config file for running the simulation
     :param scheduler_names: the schedulers to compare
     :return:
     """
@@ -19,8 +19,8 @@ def compare_algorithms(config_file, *scheduler_names):
         os.makedirs(TEMP_OUT_DIR)
 
     for scheduler_name in scheduler_names:
-        run_sumo(config_file, schedulers_name_map[scheduler_name], real_time=False,
-                 gui=False, output_file=os.path.join(TEMP_OUT_DIR, scheduler_name))
+        run_simulation_example(simulation_example, schedulers_name_map[scheduler_name], real_time=False,
+                               gui=False, output_file=os.path.join(TEMP_OUT_DIR, scheduler_name))
 
     output_files = [os.path.join(TEMP_OUT_DIR, scheduler_name) for
                     scheduler_name in scheduler_names]
