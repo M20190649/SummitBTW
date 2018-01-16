469ebfd16985be81854f9c31ae59b716fead8d91
LiranFarhi <liran123123@gmail.com>
2018-01-16 18:43:08 +0200

    Generic Support - Statistics page #120

--- management/gui/app-widgets.py
+++ management/gui/app-widgets.py
@@ -1,11 +1,12 @@
 import sys
 
+from gevent import os
+
 from management.functionality.algorithm_compare import compare_algorithms
 from simulator.simulate import run_simulation_example
 import pandas as pd
 
 from statistics.get_statistics import create_statistics
-
 
 __author__ = "Eylon Shoshan"
 
@@ -24,6 +25,7 @@ def root():
     """
     return render_template('index.html')
 
+
 @app.route('/pages/charts/index.html')
 def load_charts():
     """
@@ -39,42 +41,42 @@ def load_widgets():
 
     :return:
     """
+    # is_3_algo = False
 
-    stat = create_statistics('output.csv', True, '../../statistics/resources/tripinfo.xml','../../statistics/resources/tripinfo2.xml')
+    files = ['../../statistics/resources/tripinfo.xml', '../../statistics/resources/tripinfo1.xml', '../../statistics/resources/tripinfo2.xml']
 
-    return render_template('/pages/widgets.html', TotalTimeLoss_1 = int(stat['Total Time-Loss'][0]),
-                           TotalTimeLoss_2 = int(stat['Total Time-Loss'][1]),
-                           AverageTimeLoss_1 = int(stat['Average Time-Loss'][0]),
-                           AverageTimeLoss_2 = int(stat['Average Time-Loss'][1]),
-                           MaxTimeLoss_1 = int(stat['Max Time-Loss'][0]),
-                           MaxTimeLoss_2 = int(stat['Max Time-Loss'][1]),
-                           MinimumTimeLoss_1 = int(stat['Minimum Time-Loss'][0]),
-                           MinimumTimeLoss_2 = int(stat['Minimum Time-Loss'][1]),
-                           TotalWaitSteps_1=int(stat['Total waitSteps'][0]),
-                           TotalWaitSteps_2=int(stat['Total waitSteps'][1]),
-                           AverageWaitSteps_1=int(stat['Average waitSteps'][0]),
-                           AverageWaitSteps_2=int(stat['Average waitSteps'][1]),
-                           MaxWaitSteps_1=int(stat['Max waitSteps'][0]),
-                           MaxWaitSteps_2=int(stat['Max waitSteps'][1]),
-                           MinimumWaitSteps_1=int(stat['Minimum waitSteps'][0]),
-                           MinimumWaitSteps_2=int(stat['Minimum waitSteps'][1]),
-                           TotalDuration_1=int(stat['Total Duration'][0]),
-                           TotalDuration_2=int(stat['Total Duration'][1]),
-                           AverageDuration_1=int(stat['Average Duration'][0]),
-                           AverageDuration_2=int(stat['Average Duration'][1]),
-                           MaxDuration_1=int(stat['Max Duration'][0]),
-                           MaxDuration_2=int(stat['Max Duration'][1]),
-                           MinimumDuration_1=int(stat['Minimum Duration'][0]),
-                           MinimumDuration_2=int(stat['Minimum Duration'][1]),
-                           TotalRouteLength_1=int(stat['Total routeLength'][0]),
-                           TotalRouteLength_2=int(stat['Total routeLength'][1]),
-                           AverageRouteLength_1=int(stat['Average routeLength'][0]),
-                           AverageRouteLength_2=int(stat['Average routeLength'][1]),
-                           MaxRouteLength_1=int(stat['Max routeLength'][0]),
-                           MaxRouteLength_2=int(stat['Max routeLength'][1]),
-                           MinimumRouteLength_1=int(stat['Minimum routeLength'][0]),
-                           MinimumRouteLength_2=int(stat['Minimum routeLength'][1])
-                           )
+    stat = create_statistics('output.csv', True, *files)
+    algo = ['1', '2', '3']
+    values = {}
+    for i in range(1, len(files)+1):
+        values['Algo_' + str(i)] = algo[i - 1]
+
+        values['TotalTimeLoss_' + str(i)] = eval('int(stat[\'Total Time-Loss\'][' + (str(i - 1)) + '])')
+        values['AverageTimeLoss_' + str(i)] = eval('int(stat[\'Average Time-Loss\'][' + (str(i - 1)) + '])')
+        values['MaxTimeLoss_' + str(i)] = eval('int(stat[\'Max Time-Loss\'][' + (str(i - 1)) + '])')
+        values['MinimumTimeLoss_' + str(i)] = eval('int(stat[\'Minimum Time-Loss\'][' + (str(i - 1)) + '])')
+
+        values['TotalWaitSteps_' + str(i)] = eval('int(stat[\'Total waitSteps\'][' + (str(i - 1)) + '])')
+        values['AverageWaitSteps_' + str(i)] = eval('int(stat[\'Average waitSteps\'][' + (str(i - 1)) + '])')
+        values['MaxWaitSteps_' + str(i)] = eval('int(stat[\'Max waitSteps\'][' + (str(i - 1)) + '])')
+        values['MinimumWaitSteps_' + str(i)] = eval('int(stat[\'Minimum waitSteps\'][' + (str(i - 1)) + '])')
+
+        values['TotalDuration_' + str(i)] = eval('int(stat[\'Total Duration\'][' + (str(i - 1)) + '])')
+        values['AverageDuration_' + str(i)] = eval('int(stat[\'Average Duration\'][' + (str(i - 1)) + '])')
+        values['MaxDuration_' + str(i)] = eval('int(stat[\'Max Duration\'][' + (str(i - 1)) + '])')
+        values['MinimumDuration_' + str(i)] = eval('int(stat[\'Minimum Duration\'][' + (str(i - 1)) + '])')
+
+        values['TotalRouteLength_' + str(i)] = eval('int(stat[\'Total routeLength\'][' + (str(i - 1)) + '])')
+        values['AverageRouteLength_' + str(i)] = eval('int(stat[\'Average routeLength\'][' + (str(i - 1)) + '])')
+        values['MaxRouteLength_' + str(i)] = eval('int(stat[\'Max routeLength\'][' + (str(i - 1)) + '])')
+        values['MinimumRouteLength_' + str(i)] = eval('int(stat[\'Minimum routeLength\'][' + (str(i - 1)) + '])')
+
+    values['AlgoNum'] = ''
+
+    if len(files) == 2 :
+        values['AlgoNum'] = 'display: none'
+
+    return render_template('/pages/widgets.html', **values)
 
 
 @app.route('/run_simulation', methods=['GET', 'POST'])
