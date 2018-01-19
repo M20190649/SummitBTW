41a855cbeeb342da0b0172e599f7921f281ab398
LiranFarhi <liran123123@gmail.com>
2018-01-19 13:51:06 +0200

    Refactoring

--- management/gui/visual_statistics.py
+++ management/gui/visual_statistics.py
@@ -1,9 +1,9 @@
 from flask import render_template
-__authot__ = "Liran Farhi"
+__author__ = "Liran Farhi"
 
 def load_statistics_widgets(stats, schedulers):
     """
-    # TODO: fill documentation
+    # Reload statistics page
     :param stats:
     :param schedulers:
     :return:
@@ -16,10 +16,15 @@ def load_statistics_widgets(stats, schedulers):
     for i in range(1, num_algos + 1):
         values['Algo_' + str(i)] = schedulers[i - 1]
         for param in statistics_keys:
+            # The Name of the specific statistic
             key = param.replace("-", "").replace(" ", "").replace("waitSteps", "WaitSteps").replace("routeLength",
                                                                                                     "RouteLength")
+            # The Value of the specific statistic
             value = 'stats[\'' + param + '\'][' + (str(i - 1)) + ']'
+
             values[key + '_' + str(i)] = int(eval(value))
+
+            # Calculation the Progress Bar
             values[key + 'Bar_' + str(i)] = 'width:' + str(
                 ((eval(value) / max(stats[param])) if max(stats[param]) != 0 else 1) * 100) + '%'
 
@@ -27,11 +32,12 @@ def load_statistics_widgets(stats, schedulers):
     values['AlgoNum_1'] = ''
     values['AlgoNum_2'] = ''
 
+    # Disable 2 algorithms if we calculate statistics for one algorithm
     if num_algos == 1:
         values['AlgoNum_1'] = 'display: none'
         values['AlgoNum_2'] = 'display: none'
 
-
+    # Disable one algorithm if we calculate statistics for two algorithm
     if num_algos == 2:
         values['AlgoNum_2'] = 'display: none'
 
