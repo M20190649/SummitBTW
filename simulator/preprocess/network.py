69cc6c6ea4190dcda804ebc1d004a0b6b2f0a605
EylonSho <eylonsho999@gmail.com>
2018-06-24 18:38:51 +0300

    update path of netgen

--- simulator/preprocess/network.py
+++ simulator/preprocess/network.py
@@ -57,6 +57,7 @@ def generate_net(sumo_network_path, size):
         netgen_bin = os.path.join(os.environ['SUMO_HOME'], 'bin', 'netgenerate.exe')
         print(netgen_bin)
         print("is file here: ", netgen_bin)
+        print(subprocess.check_output(netgen_bin)
         cmd = [netgen_bin,
                '-o', sumo_network_path,
                '--default-junction-type', 'traffic_light_right_on_red',
